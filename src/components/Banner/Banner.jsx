6ec47ed25e9c755b7abe435bdb62906cb0b553a7
Jony2697 <ijony2697@gmail.com>
2025-05-01 11:19:02 +0600

    assets added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <h2>Books to freshen up your bookshelf</h2>
+                <button>View The List</button>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
